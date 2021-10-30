aa562f899d95ba588099fd07742f8d7558318a05
Kistelekig98 <Kistelekig98@users.noreply.github.com>
2021-10-30 18:30:36 +0200

    refactor

--- WorkingWithCollections/Collections.iOS/Views/Samples/Cells/HeaderCell.cs
+++ WorkingWithCollections/Collections.iOS/Views/Samples/Cells/HeaderCell.cs
@@ -11,26 +11,16 @@ namespace Collections.Touch
 		private const string BindingText = "Title Title";
 		public static readonly NSString Key = new NSString ("HeaderCell");
 		public static readonly UINib Nib;
-
-		static HeaderCell ()
-		{
-			Nib = UINib.FromName ("HeaderCell", NSBundle.MainBundle);
-		}
-
-		public HeaderCell()
-			: base(BindingText)
-		{
-		}
-
-		public HeaderCell(IntPtr handle)
-			: base(BindingText, handle)
-		{
-		}
-
 		public string Title
 		{
-			get { return MainLabel.Text; }
-			set { MainLabel.Text = value; }
+			get => MainLabel.Text;
+			set => MainLabel.Text = value;
 		}
+
+		static HeaderCell () => Nib = UINib.FromName ("HeaderCell", NSBundle.MainBundle);
+
+		public HeaderCell() : base(BindingText) { }
+
+		public HeaderCell(IntPtr handle) : base(BindingText, handle) { }
 	}
 }
--- WorkingWithCollections/Collections.iOS/Views/Samples/Cells/KittenCell.cs
+++ WorkingWithCollections/Collections.iOS/Views/Samples/Cells/KittenCell.cs
@@ -10,24 +10,20 @@ namespace Collections.Touch
   public partial class KittenCell : MvxTableViewCell
   {
     private const string BindingText = "Name Name;ImageUrl ImageUrl";
-
     private MvxImageViewLoader _imageHelper;
-
-    public KittenCell() : base(BindingText) => InitialiseImageHelper();
-
-    public KittenCell(IntPtr handle) : base(BindingText, handle) => InitialiseImageHelper();
-
     public string Name
     {
       get => MainLabel.Text;
       set => MainLabel.Text = value; 
     }
-
     public string ImageUrl
     {
       get => _imageHelper.ImageUrl;
       set => _imageHelper.ImageUrl = value;
     }
+
+    public KittenCell() : base(BindingText) => InitialiseImageHelper();
+    public KittenCell(IntPtr handle) : base(BindingText, handle) => InitialiseImageHelper();
 
     public static float GetCellHeight() => 120f;
 
